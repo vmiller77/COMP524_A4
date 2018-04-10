4db1a71f077cdfb03aa964196afdcd0c6f924ed1
vmiller77 <vnm@live.unc.edu>
2018-04-10 14:03:11 -0400

    Got it to work, but not sure if output is correct!

--- assign4.hs
+++ assign4.hs
@@ -1,22 +1,31 @@
+import Data.List
+import Data.Char
+
 main :: IO ()
 main = return ()
 
-assign4 :: String->IO [String]
-assign4 fileName=
+anagram :: String->String->Bool
+anagram w1 w2 = sort w1 == sort w2
+
+lowerString str = [ toLower loweredString | loweredString <- str]
+
+assign4 :: String->String->IO [String]
+assign4 fileName input=
     do  dictfile<-readFile fileName
-        --inputWord hardcoded for now
-        let inputWord = "cat"
+
         --list is a list of all strings in dictionary
         let list=lines dictfile
-        --ana is a list of all anagrams for the inputWord
-        let ana=[]
-        --first index of list
-        let s = [list !! 1]
-        --compare the word to each word in dictionary
-        --if word is not same length then skip
-        --if word is same length then check to see if has matching
-        --if word is anagram then add to ana
-        return (s)
+
+        --making the list of dictionary and input lowercase
+        let lowlist = [lowerString x|x<-list]
+        let lowinput = lowerString input
+
+        --ana is list of anagrams
+        --each string in lowlist that is an anagram is stored in ana
+        let ana=[ x | x<-lowlist, anagram lowinput x]
+        return ana
+
+
 --to run in command prompt do
 --ghci assign4.hs
---assign4("dictionary.txt")
+--assign4 "dictionary.txt" "word"
